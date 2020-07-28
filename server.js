c40bb9583125540de7e9827c17d43d4b082de29d
Alberto Rigail <albertojrigail@hotmail.com>
2020-07-28 17:05:37 -0500

    server

--- server.js
+++ server.js
@@ -7,14 +7,11 @@ const db = require('./config/db');
 const app = express();
 const port = 2999;
 
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, database) => {
-    if (err) return console.log(err);
-    var database = database.db('feedback-api')
-    require('./app/routes')(app, database);
-    app.listen(port, '0.0.0.0', () => {
-        console.log('We are live on ' + port);});
-});
+if (err) return console.log(err);
+var database = database.db('feedback-api')
+require('./app/routes')(app, database);
+app.listen(port, '0.0.0.0');
+console.log('We are live on ' + port);});
