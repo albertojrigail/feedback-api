b5f6c5deeb15cf93a078b2fe12edc174e76488ca
Alberto Rigail <albertojrigail@hotmail.com>
2020-08-06 16:56:02 -0500

    commit

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const port = 2999;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 MongoClient.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, database) =>{
-    app.use(express.static(__dirname ));
     if (err) return console.log(err);
     var database = database.db('feedback-api')
     require('/app/routes')(app, database);
@@ -18,7 +17,7 @@ MongoClient.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true },
 
     app.get('/', (req, res) => {
         var uid = req.params.id;
-        res.sendFile('/homepage.html');
+        res.sendFile(__dirname + '/homepage.html');
     });
     console.log('We are live on ' + port);
 });
